ce4f91e56e925e1c031d6bd06b4ddb9ebc7d7112
RasWeb <rasmuspalm@fastmail.se>
2022-04-29 10:35:48 +0200

    Added lang attribute for script and style tags

--- to-do-list/src/components/Header.vue
+++ to-do-list/src/components/Header.vue
@@ -2,10 +2,10 @@
   <h1>Header page</h1>
 </template>
 
-<script>
+<script lang="ts">
 import { Vue } from "vue-class-component";
 
 export default class Header extends Vue {}
 </script>
 
-<style></style>
+<style lang="scss"></style>
