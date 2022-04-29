84f50bea5562ab44b0fbffe45348fa731930953d
RasWeb <rasmuspalm@fastmail.se>
2022-04-29 12:49:25 +0200

    Added componen tree to ReadMe

--- to-do-list/src/App.vue
+++ to-do-list/src/App.vue
@@ -5,20 +5,13 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Header from "./components/Header.vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 @Options({
   components: {
-    HelloWorld,
     Header,
   },
 })
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-body {
-  margin: 0;
-  padding: 0;
-}
-</style>
+<style lang="scss"></style>
